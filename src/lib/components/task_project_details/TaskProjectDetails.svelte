9ab2bc1375ac9464823d158cdd285450f5947b40
wesleywil <wesleywilsonti@gmail.com>
2023-07-11 18:36:40 -0300

    added tasks page and components used in the new page

---
+++ src/lib/components/task_project_details/TaskProjectDetails.svelte
@@ -0,0 +1,13 @@
+<div class="mt-4 px-8 text-left">
+  <h1 class="text-3xl font-bold">Project Name</h1>
+  <p class="text-xs">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit quas, quos
+    amet, porro vel nisi ullam illo odio doloremque, reprehenderit incidunt
+    dolor accusantium sint? Qui quos veniam consequatur inventore dolorem.
+  </p>
+  <div class="mt-2 flex gap-2">
+    <span class="px-2 bg-blue-200 font-semibold rounded-xl">Website</span>
+    <span class="px-2 bg-green-200 font-semibold rounded-xl">Design</span>
+    <span class="px-2 bg-red-200 font-semibold rounded-xl">Development</span>
+  </div>
+</div>
