34062b69f60d02160bdb04e6fdba97882c2ac93e
wesleywil <wesleywilsonti@gmail.com>
2023-07-16 14:07:28 -0300

    updated Menu component by showing the projects in the menu navbar

--- src/lib/components/menu/Menu.svelte
+++ src/lib/components/menu/Menu.svelte
@@ -1,3 +1,16 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import type { Project } from "../../../utils/interfaces";
+
+  let projects: Project[] = [];
+  onMount(async () => {
+    const res = await fetch("http://localhost:5173/api/projects", {
+      cache: "no-cache",
+    });
+    projects = await res.json();
+  });
+</script>
+
 <div class="h-full w-48 px-4 py-2 bg-black text-slate-400">
   <div class="h-full flex flex-col items-center justify-between">
     <div class=" flex flex-col gap-12">
@@ -17,22 +30,36 @@
       </div>
       <!-- Favorites -->
       <div class="flex flex-col gap-4">
-        <h1 class="text-xl font-semibold">Favorites</h1>
+        <a href="/projects" class="text-xl font-semibold">Favorites</a>
         <div class="flex flex-col gap-2">
-          <h2>Mirage</h2>
-          <h2>Mushroom</h2>
-          <h2>Weedo</h2>
-          <h2>Lonely Walls</h2>
+          {#await projects}
+            <h2>Loading...</h2>
+          {:then projects}
+            {#each projects as project}
+              {#if project.favorite}
+                <div class="flex gap-2 items-center">
+                  <span class="bg-red-400 w-3 h-3 rounded-full" />
+                  <h2>{project.name}</h2>
+                </div>
+              {/if}
+            {/each}
+          {/await}
         </div>
       </div>
       <!-- All projects -->
       <div class="flex flex-col gap-4">
-        <h1 class="text-xl font-semibold">All Projects</h1>
+        <a href="/projects" class="text-xl font-semibold">All Projects</a>
         <div class="flex flex-col gap-2">
-          <h2>Mirage</h2>
-          <h2>Mushroom</h2>
-          <h2>Weedo</h2>
-          <h2>Lonely Walls</h2>
+          {#await projects}
+            <h2>Loading...</h2>
+          {:then projects}
+            {#each projects as project}
+              <div class="flex gap-2 items-center">
+                <span class="bg-slate-300 w-3 h-3 rounded-full" />
+                <h2>{project.name}</h2>
+              </div>
+            {/each}
+          {/await}
         </div>
       </div>
     </div>
